b4b0fd563ac00336361d2f7014547f0abb5d7d8b
jonatanjunior13-max <jonatanjunior13@gmail.com>
2026-05-24 14:28:16 -0300

    clean: remove temporary development scripts

--- strip.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs'); 
-let html = fs.readFileSync('C:/Users/jonat/Downloads/01 _ Confirma_o.html', 'utf8'); 
-html = html.replace(/data:font\/[^;]+;base64,[^\)]+/g, '...'); 
-html = html.replace(/data:image\/[^;]+;base64,[^\"]+/g, '...'); 
-fs.writeFileSync('preview.html', html);
--- strip2.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs'); 
-let html = fs.readFileSync('preview.html', 'utf8'); 
-html = html.replace(/style="[^"]+"/g, ''); 
-fs.writeFileSync('structure.html', html);
